d18c0051ed1ce7b1453b4966c321fb375e35a4eb
Ajinkya <bodadeajinkya@gmail.com>
2021-04-29 21:59:17 +0530

    Carousel component and footer component

---
+++ client/src/components/HomepageCategories/HomepageCategories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './homepage_categories.css';
+
+
+
+
+function HomepageCategories() {
+    return (
+        <div className="page-width">
+            <div className="category-spacing">
+                <div className="category-heading">Trending Categories</div>
+                <section className="main-section">
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                </section>  
+            </div>
+        </div>
+    );
+}
+
+export default HomepageCategories;
