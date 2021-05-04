e442d529b58c20ba8af50e71e709707a1b669f78
amandhunna <amandhunna.dev@gmail.com>
2021-05-04 20:26:45 +0530

    cart page initial setup

---
+++ client/src/pages/Cart/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CartList from 'CartList';
+function Cart(props) {
+    return (
+            <>
+            {/* header */}
+            <CartList />
+            {/* Cart payment */}
+            {/* Estimate shipping */}
+            {/* Recently Viewed */}
+            {/* Footer */}
+            </>
+    );
+}
+
+export default Cart;
---
+++ client/src/pages/Cart/CartPayment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CartPayment(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CartPayment;
