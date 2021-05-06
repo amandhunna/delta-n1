fcf46004a5005596732a99e08bd275e58d1371c2
Gaurav Roy <32664953+gaurav7557@users.noreply.github.com>
2021-05-06 19:46:51 +0530

    Added carousel and footer components after review (#11) (#13)
    * Added PRD and OnePager
    
    * PRD AND ONEPAGER after review
    
    * Technical requirement - frontend
    
    * Create UI MockUps.pdf
    
    * Create UI MockUps.pdf
    
    * Update UI MockUps.pdf
    
    * Carousel and footer components
    
    * Footer changes
    
    * Footer review and some other changes
    
    * Changes in Carousel and footer after review
    
    * Carousel component and footer component
    
    * Category component - homepage complete
    
    Co-authored-by: Ajinkya Pandurang Bodade <bodadeajinkya@gmail.com>

--- client/src/TestComponent.js
+++ client/src/TestComponent.js
@@ -1,7 +1,15 @@
+import CarouselContainer from './components/CarouselContainer';
+import FooterContainter from './components/Footer/FooterContainer';
+import HomepageCategories from './components/HomepageCategories';
+import './App.css';
+
 function TestComponent() {
     const {REACT_APP_TEST_COMPONENTS, REACT_APP_REACT_ENV}  = process.env;
   return (
-    <div> Test component : {REACT_APP_REACT_ENV} {REACT_APP_TEST_COMPONENTS}
+    <div> 
+	    <CarouselContainer debug={true}/>
+	    <HomepageCategories debug={true}/>
+	    <FooterContainter />
     </div>
   );
 }
