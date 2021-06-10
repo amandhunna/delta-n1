95b8d539799ddfcd2ba9783103292dd00b4fc7d7
amandhunna <amandhunna.dev@gmail.com>
2021-06-10 21:03:07 +0530

    added server code

---
+++ server/stripe.js
@@ -0,0 +1,5 @@
+const stripeAPI = require("stripe")(
+  "sk_test_51IuGEvSB2x7AywD7rBnSW7WdjjBnmxwSgiCt6xtv8yVkko1PNU7OW2XV6OqXitM1CDJY14YDQlHdz9NQrfcOjhCM00fN0eXG5K"
+);
+
+module.exports = stripeAPI;
