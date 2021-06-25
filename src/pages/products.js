79aba25ec963cbadd6be0565f3fd1d3dd53126be
amandhunna <amandhunna.dev@gmail.com>
2021-06-26 00:01:31 +0530

    product list fix

---
+++ src/pages/products.js
@@ -0,0 +1,20 @@
+// shame file
+
+const products = [
+    {   id: 1,
+        type: "sarre",
+        images: [ "https://cdn.shopify.com/s/files/1/0082/5091/6915/products/BeautyPlus_20210619131023052_save_200x.jpg?v=1624515007"]
+    }, { 
+        id: 2,
+        type: "sarre",
+        images: ["https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/2/20/1994844f-c59d-419f-84bc-5dc3c57342f51613801863942-1.jpg"]
+    }, {
+        id:3,
+        type: "jeans",
+        images: [
+            "https://media.istockphoto.com/photos/blue-jeans-isolated-with-clipping-path-picture-id600373506?k=6&m=600373506&s=612x612&w=0&h=foM-oap8DrT2jbZBF1MBvv_25xkDKiPIWztH_1yqpnI=",
+        ]
+    }
+]
+
+export default products;
